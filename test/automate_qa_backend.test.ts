0922d97912e3601f3b64c7dac8f301eb389000fa
Deepesh Rijal <deepesh.rijal@request.finance>
2025-11-20 17:20:24 +0545

    Initial commit

---
+++ test/automate_qa_backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutomateQaBackend from '../lib/automate_qa_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/automate_qa_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutomateQaBackend.AutomateQaBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
